f921ff5213955c908cc8b947f397cea703271741
HarithaKaruturi <harithakaruturi24@gmail.com>
2022-02-25 19:02:44 -0500

    modified account

--- src/Account.java
+++ src/Account.java
@@ -1,5 +1,16 @@
 public class Account {
     private String name;
+    private double balance;
+
+    public Account(String name,double balance){
+        this.name=name;
+        if(balance>0){
+            this.balance=balance;
+        }
+        else{
+            System.out.print("Balance is negative%n");
+        }
+    }
 
     public void setName(String name) {
         this.name=name;
@@ -7,6 +18,19 @@ public class Account {
 
     public String getName() {
         return name;
+    }
+
+    public void withDraw(double amount){
+        if(amount<=balance){
+            balance=balance-amount;
+        }
+        else{
+            System.out.print("withdrawal amount exceeded account balance");
+        }
+    }
+
+    public double getBalance(){
+        return balance;
     }
 
 }
--- src/AccountTest.java
+++ src/AccountTest.java
@@ -5,14 +5,20 @@ public class AccountTest {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        Account myAccount=new Account();
+        Account acc1=new Account("Jane",50.00);
+        Account acc2=new Account("John",20.00);
+        System.out.println("Initial account1 details:");
+        System.out.printf("%s account has %.2f balance%n",acc1.getName(),acc1.getBalance());
+        System.out.println("Initial account2 details:");
+        System.out.printf("%s account has %.2f balance%n",acc2.getName(),acc2.getBalance());
+        double withdrawamount=input.nextDouble();
+        acc2.withDraw(withdrawamount);
+        System.out.printf("%s account has %.2f balance%n",acc1.getName(),acc1.getBalance());
+        System.out.printf("%s account has %.2f balance%n",acc2.getName(),acc2.getBalance());
 
-        System.out.printf("Initial name is : %s%n%n", myAccount.getName());
-        System.out.println("Please enter the name:");
-        String theName=input.nextLine();
-        myAccount.setName(theName);
-        System.out.println();
-        System.out.printf("Name in object myAccount is : %n%s%n", myAccount.getName());
+
+
+
 
     }
 }
