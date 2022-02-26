bba31bc16c3e0cb93f6da21e6eca8502abb12a48
HarithaKaruturi <harithakaruturi24@gmail.com>
2022-02-25 19:07:04 -0500

    modified account test

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 
 public class AccountTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Account acc1=new Account("Jane",50.00);
-        Account acc2=new Account("John",20.00);
+        Account acc1 = new Account("Jane", 50.00);
+        Account acc2 = new Account("John", 20.00);
         System.out.println("Initial account1 details:");
-        System.out.printf("%s account has %.2f balance%n",acc1.getName(),acc1.getBalance());
+        System.out.printf("%s account has %.2f balance%n", acc1.getName(), acc1.getBalance());
         System.out.println("Initial account2 details:");
-        System.out.printf("%s account has %.2f balance%n",acc2.getName(),acc2.getBalance());
-        double withdrawamount=input.nextDouble();
+        System.out.printf("%s account has %.2f balance%n", acc2.getName(), acc2.getBalance());
+        double withdrawamount = input.nextDouble();
         acc2.withDraw(withdrawamount);
-        System.out.printf("%s account has %.2f balance%n",acc1.getName(),acc1.getBalance());
-        System.out.printf("%s account has %.2f balance%n",acc2.getName(),acc2.getBalance());
-
-
-
-
+        System.out.printf("%s account has %.2f balance%n", acc1.getName(), acc1.getBalance());
+        System.out.printf("%s account has %.2f balance%n", acc2.getName(), acc2.getBalance());
 
     }
 }
